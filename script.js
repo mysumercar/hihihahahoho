cb1302c6b67151fa3e6c8d1a0f9a1c40596ce52b
mysumercar <161153888+mysumercar@users.noreply.github.com>
2024-02-24 19:39:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function checkText() {
     // Перевірка, чи введений текст відповідає заданому
     if (inputText === 'Даша') {
         window.location.href = 'u_Best/success.html'; // Перенаправлення на іншу сторінку
-    } else {
+    } else if (inputText === 'Артем') {
+        alert('Артем А ти хулі тут'); // Виведення повідомлення про "Артем шпвпв"
+    }else {
         alert('Неправильний код-доступа до gfkgrekgkjrfdnjrn'); // Повідомлення про неправильний текст
     }
 }
